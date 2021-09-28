e0cb82402131c3eb9b9b0a8cc999b81f0eddaab0
HelloApp <stogashi146@gmail.com>
2021-09-28 15:40:22 +0900

    [add] ユーザー認証実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_include_comma
@@ -6,5 +8,9 @@ class Task < ApplicationRecord
   private
   def validate_name_not_include_comma
     errors.add(:name, "にコンマを含めることはできません") if name&.include?(",")
+  end
+
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
   end
 end
