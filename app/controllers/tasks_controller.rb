49ee432952f17a6bb38770e424d7d8a3e8a5731d
HelloApp <stogashi146@gmail.com>
2021-09-29 12:06:18 +0900

    [add] autolink実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy ]
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
+
   end
 
   def new
@@ -20,25 +21,31 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
   def update
-    task = Task.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+
 
   private
   def task_params
     params.require(:task).permit(:name, :decription)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
