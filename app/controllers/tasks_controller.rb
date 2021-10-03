5092a8281b8f88cdc77c867e5063d04ddba0fd01
HelloApp <stogashi146@gmail.com>
2021-10-03 11:34:21 +0900

    [add] 非同期削除実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -59,7 +59,8 @@ class TasksController < ApplicationController
   def destroy
 
     @task.destroy
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+    # redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+    # head :no_content
   end
 
   private
